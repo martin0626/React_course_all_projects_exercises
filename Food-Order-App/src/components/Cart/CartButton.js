18618536f58bff96e9cc8440b16a12d0cb308e54
martin0626 <scbsteel12@gmail.com>
2022-12-14 15:38:24 +0200

    Finsih Project

--- Food-Order-App/src/components/Cart/CartButton.js
+++ Food-Order-App/src/components/Cart/CartButton.js
@@ -1,17 +1,37 @@
-import react, { Fragment, useContext, useState } from "react";
+import react, { Fragment, useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import classes from "./CartButton.module.css";
 import CartContext from "../../store/cart-context";
+import { findRenderedComponentWithType } from "react-dom/test-utils";
 
 const CartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  let [isHighlited, setHighlite] = useState(false);
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+
+    const timer = setHighlite(true);
+    setTimeout(() => {
+      setHighlite(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   let itemsCount = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  let btnClasses = `${classes.button} ${isHighlited ? classes.bump : ""}`;
+
   return (
     <Fragment>
-      <button onClick={props.cartHandler} className={classes.button}>
+      <button onClick={props.cartHandler} className={btnClasses}>
         <span>
           <CartIcon />
         </span>
