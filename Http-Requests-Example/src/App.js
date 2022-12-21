eb0c850f737211a465b124ea750288bae68cf9c5
martin0626 <scbsteel12@gmail.com>
2022-12-21 15:17:28 +0200

    Finish Http requests module

--- Http-Requests-Example/src/App.js
+++ Http-Requests-Example/src/App.js
@@ -1,32 +1,81 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  let [movies, setMovies] = useState([]);
+  let [isLoading, setLoading] = useState(false);
+  let [error, setError] = useState(null);
 
+  let requestHanler = useCallback(async () => {
+    setLoading(true);
+    try {
+      let response = await fetch(
+        "https://jstest-47ca2-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
+
+      let arrayResults = [];
+
+      let data = await response.json();
+
+      for (let id in data) {
+        arrayResults.push({
+          id: data[id],
+          title: data[id].title,
+          openingText: data[id].openingText,
+          releaseDate: data[id].releaseDate,
+        });
+      }
+
+      setMovies(arrayResults);
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
+  }, []);
+
+  let addMovieHandler = async (movie) => {
+    let response = await fetch(
+      "https://jstest-47ca2-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+
+    requestHanler();
+  };
+
+  useEffect(() => {
+    requestHanler();
+  }, [requestHanler]);
+  let content = <p>No results!</p>;
+
+  if (!isLoading && movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <button onClick={requestHanler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
