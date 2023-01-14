42758eb4bc0247ed4a4feb8c9ac44699e4c25ce7
martin0626 <scbsteel12@gmail.com>
2023-01-14 21:48:08 +0200

    Added Some Changes

--- React-Router/src/pages/NewQuote.js
+++ React-Router/src/pages/NewQuote.js
@@ -1,8 +1,11 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  let history = useHistory();
   let quotesHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={quotesHandler} />;
 };
