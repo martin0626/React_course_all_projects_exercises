38b17328ede0b796c79003e211c2df17331f7f17
martin0626 <scbsteel12@gmail.com>
2023-02-03 10:42:43 +0200

    Added Css Animation

--- Animations-React/src/components/Backdrop/Backdrop.js
+++ Animations-React/src/components/Backdrop/Backdrop.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./Backdrop.css";
 
-const backdrop = (props) => <div className={props.show && "Backdrop"}></div>;
+const backdrop = (props) => (
+  <div className={props.show ? "Backdrop" : ""}></div>
+);
 
 export default backdrop;
